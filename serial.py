c54073c0f769c4e58f38e6c3c9cc817d1a43290b
Jonathan Tyler Weber <jonathantweber@gmail.com>
2023-11-03 22:42:41 +0800

    randomword and serialgenerator done

--- serial.py
+++ serial.py
@@ -19,4 +19,22 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start = 100):
+        """
+        create initial value
+        """
+        self.current_val = start
 
+    def generate(self):
+        """
+        retrieve current value and increase stored value by 1
+        """
+        val = self.current_val
+        self.current_val += 1
+        return val
+
+    def reset(self):
+        """
+        reset current value
+        """
+        self.current_val = 100
--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,24 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    def __init__(self):
+        self.word_lst = self.get_word_lst()
+
+    def get_word_lst(self):
+        word_lst = []
+        word_count = 0
+        file = open('words.txt', 'r')
+        for line in file:
+            word_lst.append(line)
+            word_count +=1
+        file.close()
+        print(f'{word_count} words read')
+        return word_lst
+
+    def get_rand_word(self):
+        random_index = random.randrange(len(self.word_lst))
+        random_word = self.word_lst[random_index].rstrip('\n')
+        return random_word
+
+
